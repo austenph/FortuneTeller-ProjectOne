45572160d47bdbb270acee16437b528da4334a66
Austen Perelman-Hall <austenph@gmail.com>
2017-01-07 22:33:28 -0500

    second commit, first and last name entered.

--- FortuneTeller-ProjectOne/Program.cs
+++ FortuneTeller-ProjectOne/Program.cs
@@ -10,6 +10,16 @@ namespace FortuneTeller_ProjectOne
     {
         static void Main(string[] args)
         {
+            //part one of my program will ask a user a series of questions
+
+            Console.WriteLine("Hello, Welcome to Fortune Teller. Please enter your first name to get started.");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Thanks " + firstName + "! Now please enter your last name.");
+            string lastName = Console.ReadLine();
+            //just got users first and last name enter 
+
+
+
         }
     }
 }
